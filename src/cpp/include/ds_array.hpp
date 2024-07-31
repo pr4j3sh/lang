6b3dc56044e5764d3819ac56920e3c50779ed71c
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-07-31 18:39:45 +0530

    added insertion

--- src/cpp/include/ds_array.hpp
+++ src/cpp/include/ds_array.hpp
@@ -7,6 +7,8 @@ using namespace std;
 string *create_array(int);
 void print_array(string *, int);
 pair<string *, int> insert_beg(string *, int, string);
+pair<string *, int> insert_end(string *, int, string);
+pair<string *, int> insert_mid(string *, int, string, int);
 void free_mem(string *);
 
 #endif
--- src/cpp/src/main.cpp
+++ src/cpp/src/main.cpp
@@ -1,6 +1,6 @@
 #include "basic.hpp"
 #include "ds_array.hpp"
-#include "iostream"
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -17,11 +17,29 @@ int main() {
   string el = "";
   cout << "> ";
   cin >> el;
-  pair<string *, int> p;
+  pair<string *, int> p, q, r;
   p = insert_beg(arr, n, el);
-  print_array(p.first, p.second);
+  n = p.second;
+  print_array(p.first, n);
+
+  cout << "> ";
+  cin >> el;
+  q = insert_end(p.first, n, el);
+  n = q.second;
+  print_array(q.first, n);
+
+  int j;
+  cout << "> ";
+  cin >> j;
+  cout << "> ";
+  cin >> el;
+  r = insert_mid(q.first, n, el, j);
+  n = r.second;
+  print_array(r.first, n);
 
   free_mem(arr);
   free_mem(p.first);
+  free_mem(q.first);
+  free_mem(r.first);
   return 0;
 }
