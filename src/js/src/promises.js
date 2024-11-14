ceb8e9601feea22bd6b152ee7f54442402fb6f96
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-14 21:03:00 +0530

    added js

---
+++ src/js/src/promises.js
@@ -0,0 +1,19 @@
+// creating a promise
+// const prom = new Promise((resolve, reject) => {
+//   const success = false;
+//   if (success) {
+//     resolve("promise resolved");
+//   } else {
+//     reject("promise rejected");
+//   }
+// });
+//
+// // consuming a promise
+// export async function consume() {
+//   try {
+//     const res = await prom;
+//     console.log(res);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
