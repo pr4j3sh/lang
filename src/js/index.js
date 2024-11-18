eade1205846bcb7599439adbcee0a2f543e3fa07
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-18 19:39:06 +0530

    cleaned js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,1 @@
-// import { consume } from "./src/promises.js";
-
-console.log("hello world");
-
-// await consume();
-
-var a = { x: 100, y: 200 };
-var b = { p: 1, q: 2 };
-
-console.log(a.x);
-console.log(a.y);
-console.log(b.p);
-console.log(b.q);
-
-console.log(b.x);
+console.log("hi");
