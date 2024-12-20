eadf2c62c44aa299b0961dee7a8ed8dbcf089659
pr4j3sh <prajesh.eleven118@gmail.com>
2024-12-21 00:08:39 +0530

    cpp: added insertion sort desc order fn

--- src/cpp/algo/sort/insertion/main.cpp
+++ src/cpp/algo/sort/insertion/main.cpp
@@ -9,20 +9,6 @@ void disp(vector<int> &v) {
   cout << endl;
 }
 
-// void asc(vector<int> &v) {
-//   int n(v.size());
-//
-//   for (int i = 1; i < n; i++) {
-//     int key = v[i];
-//     for (int j = i - 1; j >= 0; j--) {
-//       if (v[j] > key) {
-//         v[j + 1] = v[j];
-//         v[j] = key;
-//       }
-//     }
-//   }
-// }
-
 void asc(vector<int> &v) {
   int n = v.size();
 
@@ -34,16 +20,31 @@ void asc(vector<int> &v) {
       v[j + 1] = v[j];
     }
     v[j + 1] = key;
-    disp(v);
+  }
+}
+
+void desc(vector<int> &v) {
+  int n = v.size();
+
+  for (int i = 1; i < n; i++) {
+    int key = v[i];
+    int j = i - 1;
+
+    for (; j >= 0 && v[j] < key; j--) {
+      v[j + 1] = v[j];
+    }
+    v[j + 1] = key;
   }
 }
 
 int main(int argc, char *argv[]) {
 
-  vector<int> v = {13, 13, 9, 11, 3};
+  vector<int> v = {13, 12, 9, 11, 3};
 
   disp(v);
   asc(v);
+  disp(v);
+  desc(v);
   disp(v);
 
   return 0;
