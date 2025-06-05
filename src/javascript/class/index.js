32cdbe5ad29bf2589dd29e927383a2c9f38f3a51
pr4j3sh <prajesh.eleven118@gmail.com>
2025-06-05 23:56:10 +0530

    added js class

---
+++ src/javascript/class/index.js
@@ -0,0 +1,12 @@
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let obj = new Student("aryansh", 11);
+let obj2 = new Student("saumya", 21);
+
+console.log(obj.name);
+// console.log(obj2);
